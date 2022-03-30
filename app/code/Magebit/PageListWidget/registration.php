17c010c35194ace80899d67dcb502a6a79e7a346
Oskars <lejnieks.oskars@gmail.com>
2022-03-30 15:42:30 +0300

    Adding only My widget to new branch

--- app/code/Magebit/PageListWidget/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-//  This file will help register the module
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Magebit_PageListWidget',
-    __DIR__
-);
